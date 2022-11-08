55d086ed6c806df7837087b4438927c9f774a539
Brandon McClenathan <brandon@mcclenathan.us>
2022-11-08 08:33:48 -0500

    partially re-organized

--- scrape.py
+++ scrape.py
@@ -1,10 +1,8 @@
 import argparse
 import urllib.parse
 import urllib.request
-import requests
-from bs4 import BeautifulSoup
-import os
 import parseHtml
+import os
 
 def main():
     # ----- Getting inputs from command line -----
@@ -17,6 +15,7 @@ def main():
         required=False,
         metavar='LINK',
         help='Apartments.com link to parse.')
+
     # Update command line argument
     parser.add_argument(
         '-u',
@@ -30,28 +29,25 @@ def main():
         )
     args = parser.parse_args()
 
+    # ----- Defining the shared file paths -----
+    ulrFilePath = os.path.abspath('/Users/brandonmcclenathan/Library/Mobile Documents/com~apple~CloudDocs/Documents/Code/apartmentScraper - Shared Files/apartmentLinks.txt')
+    
+
     # If a link was specified
     if args.link != None:
-        scrapeLink(args.link)
+        parsedLink = urllib.parse.urlparse(args.link)        # Parsing the link to get the network location
+        if parsedLink.netloc != 'www.apartments.com':   # If the link is not located at apartments.com, raise an exception
+            raise Exception('Error: Incompatible Link')
+        
+        with open(ulrFilePath) as f:
+            pass
 
     # If update was specified
     if args.update == 'yes':
         updateSheet()
-
-# ---------- Functions ----------
+    
 def scrapeLink(link):
-    parsedLink = urllib.parse.urlparse(link)        # Parsing the link to get the network location
-    if parsedLink.netloc != 'www.apartments.com':   # If the link is not located at apartments.com, raise an exception
-        raise Exception('Error: Incompatible Link')
-
-    # Defining the headers
-    HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
-    }
-    # Pulling data from apartments.com
-    htmlText = requests.get(link,timeout=5,headers=HEADERS).text
-    # Parsing the html text to get the info from site
-    parseHtml.parseHtml(htmlText)
+    apartment = parseHtml.parseHtml(link)
     
 
 def updateSheet():
